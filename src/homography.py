2aaaefe6ecfa4bd2b39c7ad948f1ac42af514224
joposada7 <joposada7@gmail.com>
2023-04-30 16:35:50 -0400

    implement homography matrix

--- src/homography.py
+++ src/homography.py
@@ -7,6 +7,30 @@ import cv2
 from geometry_msgs.msg import PoseStamped
 from visualization_msgs.msg import Marker
 
+##############################################
+# pixels (i,j), images taken from left camera sensor
+PTS_IMAGE_PLANE = [[223, 321],
+                   [366, 294],
+                   [515, 328],
+                   [155, 274],
+                   [285, 260],
+                   [379, 246],
+                   [524, 253],
+                   [300, 218],
+                   [388, 219]]
+
+# inches (x,y), where x is forward and y is left of left camera sensor
+PTS_GROUND_PLANE = [[15.5,   4.0],
+                    [17.75, -3.5],
+                    [13.0,  -9.5],
+                    [22.0,   10.75],
+                    [24.5,   2.0],
+                    [28.0,  -5.5],
+                    [24.5,  -16.0],
+                    [41.75,  2.25],
+                    [39.5,  -8.0]]
+##############################################
+
 class HomographyTransformer:
     """
     Takes lanes detected and returns the best lookahead point for the pure
@@ -17,13 +41,34 @@ class HomographyTransformer:
     """
 
     def __init__(self):
-        pass
+        # Initialize homography matrix
+        if not len(PTS_GROUND_PLANE) == len(PTS_IMAGE_PLANE):
+            rospy.logerr("ERROR: PTS_GROUND_PLANE and PTS_IMAGE_PLANE should be of same length")
+
+        np_pts_ground = np.array(PTS_GROUND_PLANE)
+        np_pts_ground = np_pts_ground * METERS_PER_INCH
+        np_pts_ground = np.float32(np_pts_ground[:, np.newaxis, :])
+
+        np_pts_image = np.array(PTS_IMAGE_PLANE)
+        np_pts_image = np_pts_image * 1.0
+        np_pts_image = np.float32(np_pts_image[:, np.newaxis, :])
+
+        self.h, err = cv2.findHomography(np_pts_image, np_pts_ground)
 
     def find_lookahead_point(self):
         pass
 
-    def pixel_to_world(self):
-        pass
+    def pixel_to_world(self, i, j):
+        """
+        Takes pixel coordinates (i,j) and returns world frame coordinates (x,y) in meters.
+        """
+        homogeneous_point = np.array([[u], [v], [1]])
+        xy = np.dot(self.h, homogeneous_point)
+        scaling_factor = 1.0 / xy[2, 0]
+        homogeneous_xy = xy * scaling_factor
+        x = homogeneous_xy[0, 0]
+        y = homogeneous_xy[1, 0]
+        return (x, y)
 
     def transform_to_base_link(self):
         pass
