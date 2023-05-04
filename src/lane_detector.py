223fda46d46de496db9fff2ae0752d9d1c1258e5
wellscrosby <wellscrosby@gmail.com>
2023-05-03 21:40:03 -0400

    image downsampling

--- src/lane_detector.py
+++ src/lane_detector.py
@@ -5,10 +5,12 @@ import numpy as np
 import cv2 as cv
 from cv_bridge import CvBridge, CvBridgeError
 import copy
+from skimage.transform import downscale_local_mean
 
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Point
 from lane_follower.msg import Lane
+
 
 class LaneDetector:
     """
@@ -24,26 +26,35 @@ class LaneDetector:
 
         # For zed camera
         self.bridge = CvBridge()
-        self.camera_sub = rospy.Subscriber("/zed/zed_node/rgb/image_rect_color", Image, self.detect_lane)
+        self.camera_sub = rospy.Subscriber(
+            "/zed/zed_node/rgb/image_rect_color", Image, self.detect_lane
+        )
 
-    def detect_lane(self,image):
-        img = self.bridge.imgmsg_to_cv2(image, "bgr8")
+    def detect_lane(self, image):
+        img = downscale_local_mean(self.bridge.imgmsg_to_cv2(image, "bgr8"), (2, 2))
 
         # Mask top half of image
         top_half_mask = np.zeros_like(img)
         height = img.shape[0]
-        height_part = int(np.floor(height*0.5))
-        top_half_mask[height_part:,:] = 255
-        blacked_img = cv.bitwise_and(img,top_half_mask)
+        height_part = int(np.floor(height * 0.5))
+        top_half_mask[height_part:, :] = 255
+        blacked_img = cv.bitwise_and(img, top_half_mask)
 
-        # Grayscale and blur image to get the edges 
-        gray_img = cv.cvtColor(blacked_img,cv.COLOR_BGR2GRAY)
-        white_thresh = cv.threshold(gray_img,150,255,cv.THRESH_BINARY)[1]
-        blur = cv.GaussianBlur(white_thresh,(5,5),0)
-        edges = cv.Canny(blur,50, 150, apertureSize=3)
+        # Grayscale and blur image to get the edges
+        gray_img = cv.cvtColor(blacked_img, cv.COLOR_BGR2GRAY)
+        white_thresh = cv.threshold(gray_img, 150, 255, cv.THRESH_BINARY)[1]
+        blur = cv.GaussianBlur(white_thresh, (5, 5), 0)
+        edges = cv.Canny(blur, 50, 150, apertureSize=3)
 
         # Get the lines from opencv
-        lines = cv.HoughLinesP(edges,rho=1,theta=np.pi/180,threshold=50, minLineLength=150,maxLineGap=10)
+        lines = cv.HoughLinesP(
+            edges,
+            rho=1,
+            theta=np.pi / 180,
+            threshold=50,
+            minLineLength=150,
+            maxLineGap=10,
+        )
 
         msg = Lane()
         msg.header.stamp = rospy.Time.now()
@@ -58,14 +69,14 @@ class LaneDetector:
         # Filter lines based on their angle, aiming for almost vertical lines
         filtered_lines = []
         for line in lines:
-            x1,y1,x2,y2 = line[0]
+            x1, y1, x2, y2 = line[0]
             # angle = np.arctan2(y2-y1, x2-x1) * 180 / np.pi
-            if abs(y1-y2) < 40:
+            if abs(y1 - y2) < 40:
                 continue
             # if abs(angle) < 30: # vertical lines
             else:
                 filtered_lines.append(list(line[0]))
-                    
+
         """
         Filter lines again, this time to try to get a single line per lane.
         Basically, I am checking all the lines to see if there are lines which have very similar slopes, and if so,
@@ -74,13 +85,13 @@ class LaneDetector:
         slope_tracker = {}
         copy_filt = copy.deepcopy(filtered_lines)
         for line in copy_filt:
-            x1,y1,x2,y2 = line
-            slope = (y2-y1)/(x2-x1)
+            x1, y1, x2, y2 = line
+            slope = (y2 - y1) / (x2 - x1)
             get_rid = False
             if slope_tracker != {}:
                 copy_slopes = copy.deepcopy(slope_tracker)
                 for oth_slope in copy_slopes.keys():
-                    if abs(slope-oth_slope) < 0.20:
+                    if abs(slope - oth_slope) < 0.20:
                         if slope < 0:
                             if x1 < slope_tracker[oth_slope][0]:
                                 filtered_lines.remove(slope_tracker[oth_slope])
@@ -106,19 +117,19 @@ class LaneDetector:
         print(slope_tracker)
         """
         rospy.loginfo(str(len(filtered_lines)) + " lines found.")
-        
-        y_return =  int(np.floor(height * self.LOOKAHEAD_HOMOGRAPHY))
+
+        y_return = int(np.floor(height * self.LOOKAHEAD_HOMOGRAPHY))
         if len(filtered_lines) == 2:
             # Publish both lines
-            x1,y1,x2,y2 = filtered_lines[0]
-            m_1 = float(y2-y1)/(x2-x1)
-            b = y1 - m_1*x1
-            x_lane_1 = (y_return-b)/m_1
+            x1, y1, x2, y2 = filtered_lines[0]
+            m_1 = float(y2 - y1) / (x2 - x1)
+            b = y1 - m_1 * x1
+            x_lane_1 = (y_return - b) / m_1
 
-            x1,y1,x2,y2 = filtered_lines[1]
-            m_2 = float(y2-y1)/(x2-x1)
-            b = y1 - m_2*x1
-            x_lane_2 = (y_return-b)/m_2
+            x1, y1, x2, y2 = filtered_lines[1]
+            m_2 = float(y2 - y1) / (x2 - x1)
+            b = y1 - m_2 * x1
+            x_lane_2 = (y_return - b) / m_2
 
             # x_return = int(np.floor((x_lane_1+x_lane_2)/2))
             # cv.circle(img, (x_return,y_return), 5, (0, 0, 255), -1)
@@ -141,10 +152,10 @@ class LaneDetector:
 
         elif len(filtered_lines) == 1:
             # Publish only one line
-            x1,y1,x2,y2 = filtered_lines[0]
-            m_1 = (y2-y1)/(x2-x1)
-            b = y1 - m_1*x1
-            x_lane_1 = (y_return-b)/m_1
+            x1, y1, x2, y2 = filtered_lines[0]
+            m_1 = (y2 - y1) / (x2 - x1)
+            b = y1 - m_1 * x1
+            x_lane_1 = (y_return - b) / m_1
 
             p1 = Point()
             p1.x = x_lane_1
@@ -198,14 +209,13 @@ class LaneDetector:
         cv.destroyAllWindows()
         """
 
-
-
     # IDEAS
     """
     # Rrturn line locations after first call back, use these to see where we at
     # Make the line, return it, next time if theres only one, we peep if that one is close by the previous
     # I think homography needs to do the mean of the lines, the pixel mean is probably not accurate for shit
     """
+
 
 if __name__ == "__main__":
     rospy.init_node("lane_detector")
